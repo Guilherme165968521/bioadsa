25c2753ea7b35d1fee3c4f49e6377e6eaad34e7c
Guilherme165968521 <guilnhermedesouzaferreira8@gmail.com>
2025-04-14 15:35:07 -0300

    first commit

---
+++ aa.bat
@@ -0,0 +1,7 @@
+echo "# bioadsa" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Guilherme165968521/bioadsa.git
+git push -u origin main
